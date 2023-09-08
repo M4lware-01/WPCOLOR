9b8f517b301c05df4ed59d98eaa4732d6d59c1f6
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-09-08 22:56:04 +0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,12 @@
-git clone https://github.com/M4lware-01/WPCOLOR.git
+$INSTALL_DIR = [System.Environment]::GetFolderPath('ApplicationData')
 
-Set-Location -Path ".\WPCOLOR"
+$LIB_DIR = "WP_lib"
+
+$FULL_INSTALL_DIR = Join-Path -Path $INSTALL_DIR -ChildPath $LIB_DIR
+
+git clone https://github.com/M4lware-01/WPCOLOR.git $FULL_INSTALL_DIR
+
+Set-Location -Path $FULL_INSTALL_DIR
 
 pip install -e .
 
