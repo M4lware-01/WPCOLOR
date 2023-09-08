6817f1eea1053090e7e4654a3f0fe591817efea2
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-09-08 23:18:29 +0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,6 +3,7 @@ $destination = Join-Path -Path $env:APPDATA -ChildPath 'WP_lib'
 if (-not (Test-Path -Path $destination -PathType Container)) {
     New-Item -Path $destination -ItemType Directory
 }
+
 Invoke-WebRequest -Uri 'https://github.com/M4lware-01/WPCOLOR/archive/main.zip' -OutFile (Join-Path -Path $destination -ChildPath 'WPCOLOR-main.zip')
 
 Set-Location -Path $destination
@@ -11,6 +12,13 @@ Expand-Archive -Path 'WPCOLOR-main.zip' -DestinationPath .
 
 Rename-Item -Path 'WPCOLOR-main' -NewName 'WPCOLOR'
 
-Remove-Item -Path 'WPCOLOR-main.zip' -Force
+Set-Location -Path 'WPCOLOR'
 
-Write-Host "Téléchargement, extraction et décompression terminés."
+python setup.py sdist bdist_wheel
+
+pip install .
+
+Remove-Item -Path (Join-Path -Path $destination -ChildPath 'WPCOLOR-main.zip') -Force
+
+Write-Host "Téléchargement, extraction, décompression et installation terminés."
+
