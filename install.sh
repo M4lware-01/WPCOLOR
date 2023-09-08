c0b669c434433d1876153ad38553e9fda382f837
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-09-08 22:55:49 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-sudo apt install git -y
-git clone https://github.com/M4lware-01/WPCOLOR.git
+INSTALL_DIR="/usr/share/doc"
+LIB_DIR="WP_lib"
+FULL_INSTALL_DIR="$INSTALL_DIR/$LIB_DIR"
 
-cd WPCOLOR
+mkdir -p "$FULL_INSTALL_DIR"
+
+cd "$FULL_INSTALL_DIR"
+
+git clone https://github.com/M4lware-01/WPCOLOR.git .
 
 pip install -e .
 
