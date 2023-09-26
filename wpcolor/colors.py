3f28dd863aef7527b5d2db5a862ded5eeefe65bb
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-09-26 20:16:25 +0200

    Update colors.py

--- wpcolor/colors.py
+++ wpcolor/colors.py
@@ -1,5 +1,7 @@
 class WP:
+    # Normal colors
     RESET = '\033[0m'
+    BLACK = '\033[30m'
     RED = '\033[31m'
     GREEN = '\033[32m'
     YELLOW = '\033[33m'
@@ -7,3 +9,14 @@ class WP:
     MAGENTA = '\033[35m'
     CYAN = '\033[36m'
     WHITE = '\033[37m'
+    
+    # Light colors
+
+    LG_BLACK = '\033[90m'
+    LG_RED = '\033[91m'
+    LG_GREEN = '\033[92m'
+    LG_YELLOW = '\033[93m'
+    LG_BLUE = '\033[94m'
+    LG_MAGENTA = '\033[95m'
+    LG_CYAN = '\033[96m'
+    LG_WHITE = '\033[97m'
