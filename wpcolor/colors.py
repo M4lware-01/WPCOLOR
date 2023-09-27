cc7241a02d8df956e265b7bedc4bdad4bd32ec16
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-09-27 08:06:29 +0200

    Update colors.py

--- wpcolor/colors.py
+++ wpcolor/colors.py
@@ -9,14 +9,23 @@ class WP:
     MAGENTA = '\033[35m'
     CYAN = '\033[36m'
     WHITE = '\033[37m'
+    GREY = '\033[90m'
     
-    # Light colors
+    # light colors
 
-    LG_BLACK = '\033[90m'
     LG_RED = '\033[91m'
     LG_GREEN = '\033[92m'
     LG_YELLOW = '\033[93m'
     LG_BLUE = '\033[94m'
     LG_MAGENTA = '\033[95m'
     LG_CYAN = '\033[96m'
-    LG_WHITE = '\033[97m'
+
+    # RGB
+
+    def RGB(self,string):
+        dict,x,output = {1:f"{WP.RED}",2:f"{WP.YELLOW}",3:f"{WP.LG_YELLOW}",4:f"{WP.LG_GREEN}",5:f"{WP.CYAN}",6:f"{WP.LG_BLUE}",7:f"{WP.BLUE}",8:f"{WP.MAGENTA}",9:f"{WP.LG_MAGENTA}"},0,''
+        for i,char in enumerate(string):
+            x=1 if x>len(dict)-1 else x+1
+            output+=dict[x]+char
+        return output
+        
